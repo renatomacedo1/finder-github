9e5d1758609ef60fbef17adc0b00f90c97bf7cd1
renatomacedo1 <macedo.renato@protonmail.com>
2019-12-13 22:58:52 +0000

    Version 1.0.0 deployed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,33 @@
-import React, { Component } from "react";
-import ".github-finder/src/App.css";
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className="fab fa-github" /> Navbar
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github"
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
